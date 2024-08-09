b3199103d100298e855543d3a0fab07f9e5cbd02
deuwin <wcully@gmail.com>
2024-08-09 19:04:21 +0100

    Add argparse to nml generator

--- src/python/generate_nml.py
+++ src/python/generate_nml.py
@@ -1,30 +1,65 @@
 from pathlib import Path
 import sys
 import os
+import argparse
 
 from industry import generateIndustryNml
 from industry_functions import genIsStockpileFull
+from cargo import generateCargoTable
+
 
 def errorExit(error):
     print(f"\033[91mError\033[0m {str(error)}", file=sys.stderr)
     sys.exit(1)
 
 
-def main(argv):
-    if len(argv) == 1:
-        errorExit("Output directory not specified!")
+def parseArguments():
+    parser = argparse.ArgumentParser(
+        description=(
+            "Generate .pnml files for use in dSPI.grf. Called without arguments "
+            "will create files in the current directory."
+        )
+    )
+    parser.add_argument(
+        "-d",
+        "--output-directory",
+        help="Directory to place generated .pnml files",
+        default=".",
+        type=Path,
+    )
+    parser.add_argument(
+        "-l",
+        "--list-files",
+        help="List output files that would be generated but do not generate them",
+        action="store_true",
+    )
+    args = parser.parse_args()
 
-    output_directory = Path(argv[1])
-    if not output_directory.is_dir():
-        errorExit(f'Specified path "{output_directory}" is not a directory!')
+    return args
 
-    output_files = {
-        "industry.pnml": genIsStockpileFull,
-        "industry_functions.pnml": generateIndustryNml
-    }
+def generateIndustry():
+    nml = genIsStockpileFull()
+    nml += generateIndustryNml()
+    return nml
+
+_OUTPUT_FILES = {
+    "cargotable.pnml": generateCargoTable,
+    "industry.pnml": generateIndustry,
+}
+
+def main():
+    args = parseArguments()
+
+    if args.list_files:
+        files = []
+        for file in _OUTPUT_FILES.keys():
+            files.append(str(args.output_directory / file))
+        print(" ".join(files))
+        exit(os.EX_OK)
+
     try:
-        for file, generator in output_files.items():
-            file_out = output_directory / file
+        for file, generator in _OUTPUT_FILES.items():
+            file_out = args.output_directory / file
             file_out.write_text(generator())
     except OSError as err:
         errorExit(err)
@@ -33,4 +68,4 @@ def main(argv):
 
 
 if __name__ == "__main__":
-    sys.exit(main(sys.argv))
+    sys.exit(main())
